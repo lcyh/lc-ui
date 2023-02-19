fa7a13838c83501200a64b5c4ec8d2198a7b844f
lcyh <lcyh@users.noreply.github.com>
2023-02-19 00:53:06 +0000

    deploy: d1b692b13c780dd9412f6e9a9f5290a32b249749

---
+++ assets/components_count-to_index.md.feb7c0f9.lean.js
@@ -0,0 +1,49 @@
+import{V as r,_ as g,o as _,c as f,a as i,w as o,b as F,d as t,e as n,r as k}from"./app.6317ff3e.js";const E={name:"component-doc",components:{"render-demo-0":function(){const{toDisplayString:e,createElementVNode:u,resolveComponent:c,createVNode:p,openBlock:l,createElementBlock:d}=r;function a(m,V){const C=c("c-count-to");return l(),d("div",null,[u("div",null,e(m.msg),1),p(C,{prefix:"$",color:"#409EFF",startVal:1,endVal:2e5,duration:8e3})])}const{defineComponent:s}=r,h=s({setup(){return{msg:"CountTo Count-to  \u7EC4\u4EF6\u6587\u6863\u793A\u4F8B"}}});return{render:a,...h}}()}},A=JSON.parse('{"title":"CountTo Count-to","description":"","frontmatter":{},"headers":[{"level":3,"title":"\u4F55\u65F6\u4F7F\u7528","slug":"\u4F55\u65F6\u4F7F\u7528"},{"level":3,"title":"\u57FA\u672C\u7528\u6CD5","slug":"\u57FA\u672C\u7528\u6CD5"},{"level":3,"title":"c-count-to","slug":"c-count-to"}],"relativePath":"components/count-to/index.md","lastUpdated":1676767932000}'),v=F('<h1 id="countto-count-to" tabindex="-1">CountTo Count-to <a class="header-anchor" href="#countto-count-to" aria-hidden="true">#</a></h1><p>// todo \u7EC4\u4EF6\u63CF\u8FF0</p><h3 id="\u4F55\u65F6\u4F7F\u7528" tabindex="-1">\u4F55\u65F6\u4F7F\u7528 <a class="header-anchor" href="#\u4F55\u65F6\u4F7F\u7528" aria-hidden="true">#</a></h3><p>// todo \u4F7F\u7528\u65F6\u673A\u63CF\u8FF0</p><h3 id="\u57FA\u672C\u7528\u6CD5" tabindex="-1">\u57FA\u672C\u7528\u6CD5 <a class="header-anchor" href="#\u57FA\u672C\u7528\u6CD5" aria-hidden="true">#</a></h3><p>// todo \u7528\u6CD5\u63CF\u8FF0</p>',6),y=t("div",null,"// todo \u5C55\u5F00\u4EE3\u7801\u7684\u5185\u90E8\u63CF\u8FF0",-1),B=t("div",{class:"language-vue"},[t("pre",null,[t("code",null,[t("span",{class:"token tag"},[t("span",{class:"token tag"},[t("span",{class:"token punctuation"},"<"),n("template")]),t("span",{class:"token punctuation"},">")]),n(`
+  `),t("span",{class:"token tag"},[t("span",{class:"token tag"},[t("span",{class:"token punctuation"},"<"),n("div")]),t("span",{class:"token punctuation"},">")]),n("{{ msg }}"),t("span",{class:"token tag"},[t("span",{class:"token tag"},[t("span",{class:"token punctuation"},"</"),n("div")]),t("span",{class:"token punctuation"},">")]),n(`
+  `),t("span",{class:"token tag"},[t("span",{class:"token tag"},[t("span",{class:"token punctuation"},"<"),n("c-count-to")]),n(`
+    `),t("span",{class:"token attr-name"},"prefix"),t("span",{class:"token attr-value"},[t("span",{class:"token punctuation attr-equals"},"="),t("span",{class:"token punctuation"},'"'),n("$"),t("span",{class:"token punctuation"},'"')]),n(`
+    `),t("span",{class:"token attr-name"},":color"),t("span",{class:"token attr-value"},[t("span",{class:"token punctuation attr-equals"},"="),t("span",{class:"token punctuation"},'"'),t("span",{class:"token punctuation"},"'"),n("#409EFF"),t("span",{class:"token punctuation"},"'"),t("span",{class:"token punctuation"},'"')]),n(`
+    `),t("span",{class:"token attr-name"},":startVal"),t("span",{class:"token attr-value"},[t("span",{class:"token punctuation attr-equals"},"="),t("span",{class:"token punctuation"},'"'),n("1"),t("span",{class:"token punctuation"},'"')]),n(`
+    `),t("span",{class:"token attr-name"},":endVal"),t("span",{class:"token attr-value"},[t("span",{class:"token punctuation attr-equals"},"="),t("span",{class:"token punctuation"},'"'),n("200000"),t("span",{class:"token punctuation"},'"')]),n(`
+    `),t("span",{class:"token attr-name"},":duration"),t("span",{class:"token attr-value"},[t("span",{class:"token punctuation attr-equals"},"="),t("span",{class:"token punctuation"},'"'),n("8000"),t("span",{class:"token punctuation"},'"')]),n(`
+  `),t("span",{class:"token punctuation"},"/>")]),n(`
+`),t("span",{class:"token tag"},[t("span",{class:"token tag"},[t("span",{class:"token punctuation"},"</"),n("template")]),t("span",{class:"token punctuation"},">")]),n(`
+
+`),t("span",{class:"token tag"},[t("span",{class:"token tag"},[t("span",{class:"token punctuation"},"<"),n("script")]),t("span",{class:"token punctuation"},">")]),t("span",{class:"token script"},[t("span",{class:"token language-javascript"},[n(`
+`),t("span",{class:"token keyword"},"import"),n(),t("span",{class:"token punctuation"},"{"),n(" defineComponent "),t("span",{class:"token punctuation"},"}"),n(),t("span",{class:"token keyword"},"from"),n(),t("span",{class:"token string"},"'vue'"),t("span",{class:"token punctuation"},";"),n(`
+
+`),t("span",{class:"token keyword"},"export"),n(),t("span",{class:"token keyword"},"default"),n(),t("span",{class:"token function"},"defineComponent"),t("span",{class:"token punctuation"},"("),t("span",{class:"token punctuation"},"{"),n(`
+  `),t("span",{class:"token function"},"setup"),t("span",{class:"token punctuation"},"("),t("span",{class:"token punctuation"},")"),n(),t("span",{class:"token punctuation"},"{"),n(`
+    `),t("span",{class:"token keyword"},"return"),n(),t("span",{class:"token punctuation"},"{"),n(`
+      `),t("span",{class:"token literal-property property"},"msg"),t("span",{class:"token operator"},":"),n(),t("span",{class:"token string"},"'CountTo Count-to  \u7EC4\u4EF6\u6587\u6863\u793A\u4F8B'"),n(`
+    `),t("span",{class:"token punctuation"},"}"),t("span",{class:"token punctuation"},";"),n(`
+  `),t("span",{class:"token punctuation"},"}"),n(`
+`),t("span",{class:"token punctuation"},"}"),t("span",{class:"token punctuation"},")"),t("span",{class:"token punctuation"},";"),n(`
+`)])]),t("span",{class:"token tag"},[t("span",{class:"token tag"},[t("span",{class:"token punctuation"},"</"),n("script")]),t("span",{class:"token punctuation"},">")]),n(`
+
+`),t("span",{class:"token tag"},[t("span",{class:"token tag"},[t("span",{class:"token punctuation"},"<"),n("style")]),t("span",{class:"token punctuation"},">")]),t("span",{class:"token style"}),t("span",{class:"token tag"},[t("span",{class:"token tag"},[t("span",{class:"token punctuation"},"</"),n("style")]),t("span",{class:"token punctuation"},">")]),n(`
+`)])])],-1),b=F('<h3 id="c-count-to" tabindex="-1">c-count-to <a class="header-anchor" href="#c-count-to" aria-hidden="true">#</a></h3><p>c-count-to \u53C2\u6570</p><table><thead><tr><th>\u53C2\u6570</th><th>\u7C7B\u578B</th><th>\u9ED8\u8BA4</th><th>\u8BF4\u660E</th></tr></thead><tbody><tr><td></td><td></td><td></td><td></td></tr><tr><td></td><td></td><td></td><td></td></tr><tr><td></td><td></td><td></td><td></td></tr></tbody></table><p>c-count-to \u4E8B\u4EF6</p><table><thead><tr><th>\u4E8B\u4EF6</th><th>\u7C7B\u578B</th><th>\u8BF4\u660E</th></tr></thead><tbody><tr><td></td><td></td><td></td></tr><tr><td></td><td></td><td></td></tr><tr><td></td><td></td><td></td></tr></tbody></table><p>c-count-to \u63D2\u69FD</p><table><thead><tr><th>\u63D2\u69FD\u540D</th><th>\u8BF4\u660E</th></tr></thead><tbody><tr><td></td><td></td></tr><tr><td></td><td></td></tr><tr><td></td><td></td></tr></tbody></table>',7);function x(e,u,c,p,l,d){const a=k("render-demo-0"),s=k("demo");return _(),f("div",null,[v,i(s,{sourceCode:`<template>
+  <div>{{ msg }}</div>
+  <c-count-to
+    prefix="$"
+    :color="'#409EFF'"
+    :startVal="1"
+    :endVal="200000"
+    :duration="8000"
+  />
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  setup() {
+    return {
+      msg: 'CountTo Count-to  \u7EC4\u4EF6\u6587\u6863\u793A\u4F8B'
+    };
+  }
+});
+<\/script>
+
+<style></style>
+`},{description:o(()=>[y]),highlight:o(()=>[B]),default:o(()=>[i(a)]),_:1}),b])}const N=g(E,[["render",x]]);export{A as __pageData,N as default};
