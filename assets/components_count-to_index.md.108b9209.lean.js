bbe3fb06748b215679e12332f6fab4b2bb21c3d3
lcyh <lcyh@users.noreply.github.com>
2023-07-07 06:23:41 +0000

    deploy: f8dfd80b710ba14492f1f04b3aac35af25d3f5b5

---
+++ assets/components_count-to_index.md.108b9209.lean.js
@@ -0,0 +1,48 @@
+import{a4 as n,_ as A,o as E,c as B,G as p,B as l,R as i,z as t,a as o,D}from"./chunks/framework.82d16950.js";const{defineComponent:f}=n,_=f({name:"component-doc",components:{"render-demo-0":function(){const{toDisplayString:a,createElementVNode:r,resolveComponent:c,createVNode:u,Fragment:d,openBlock:F,createElementBlock:s}=n;function e(m,k){const h=c("c-count-to");return F(),s(d,null,[r("div",null,a(m.msg),1),u(h,{prefix:"$",color:"#409EFF",startVal:1,endVal:2e5,duration:8e3})],64)}const{defineComponent:y}=n,C=y({setup(){return{msg:"CountTo Count-to  组件文档示例"}}});return{render:e,...C}}()}}),q=JSON.parse('{"title":"CountTo 数字动画","description":"","frontmatter":{},"headers":[],"relativePath":"components/count-to/index.md","filePath":"components/count-to/index.md","lastUpdated":1688710937000}'),b=i('<h1 id="countto-数字动画" tabindex="-1">CountTo 数字动画 <a class="header-anchor" href="#countto-数字动画" aria-label="Permalink to &quot;CountTo 数字动画&quot;">​</a></h1><p>// todo 组件描述</p><h3 id="何时使用" tabindex="-1">何时使用 <a class="header-anchor" href="#何时使用" aria-label="Permalink to &quot;何时使用&quot;">​</a></h3><p>// todo 使用时机描述</p><h3 id="基本用法" tabindex="-1">基本用法 <a class="header-anchor" href="#基本用法" aria-label="Permalink to &quot;基本用法&quot;">​</a></h3><p>// todo 用法描述</p>',6),g=t("div",null,"// todo 展开代码的内部描述",-1),x=t("div",{class:"language-vue"},[t("pre",{"v-pre":"",class:"shiki material-theme-palenight"},[t("code",null,[t("span",{class:"line"},[t("span",{style:{color:"#89DDFF"}},"<"),t("span",{style:{color:"#F07178"}},"template"),t("span",{style:{color:"#89DDFF"}},">")]),o(`
+`),t("span",{class:"line"},[t("span",{style:{color:"#A6ACCD"}},"  "),t("span",{style:{color:"#89DDFF"}},"<"),t("span",{style:{color:"#F07178"}},"div"),t("span",{style:{color:"#89DDFF"}},">"),t("span",{style:{color:"#A6ACCD"}},"{{ msg }}"),t("span",{style:{color:"#89DDFF"}},"</"),t("span",{style:{color:"#F07178"}},"div"),t("span",{style:{color:"#89DDFF"}},">")]),o(`
+`),t("span",{class:"line"},[t("span",{style:{color:"#A6ACCD"}},"  "),t("span",{style:{color:"#89DDFF"}},"<"),t("span",{style:{color:"#F07178"}},"c-count-to")]),o(`
+`),t("span",{class:"line"},[t("span",{style:{color:"#89DDFF"}},"    "),t("span",{style:{color:"#C792EA"}},"prefix"),t("span",{style:{color:"#89DDFF"}},"="),t("span",{style:{color:"#89DDFF"}},'"'),t("span",{style:{color:"#C3E88D"}},"$"),t("span",{style:{color:"#89DDFF"}},'"')]),o(`
+`),t("span",{class:"line"},[t("span",{style:{color:"#89DDFF"}},"    "),t("span",{style:{color:"#C792EA"}},":color"),t("span",{style:{color:"#89DDFF"}},"="),t("span",{style:{color:"#89DDFF"}},'"'),t("span",{style:{color:"#C3E88D"}},"'#409EFF'"),t("span",{style:{color:"#89DDFF"}},'"')]),o(`
+`),t("span",{class:"line"},[t("span",{style:{color:"#89DDFF"}},"    "),t("span",{style:{color:"#C792EA"}},":startVal"),t("span",{style:{color:"#89DDFF"}},"="),t("span",{style:{color:"#89DDFF"}},'"'),t("span",{style:{color:"#C3E88D"}},"1"),t("span",{style:{color:"#89DDFF"}},'"')]),o(`
+`),t("span",{class:"line"},[t("span",{style:{color:"#89DDFF"}},"    "),t("span",{style:{color:"#C792EA"}},":endVal"),t("span",{style:{color:"#89DDFF"}},"="),t("span",{style:{color:"#89DDFF"}},'"'),t("span",{style:{color:"#C3E88D"}},"200000"),t("span",{style:{color:"#89DDFF"}},'"')]),o(`
+`),t("span",{class:"line"},[t("span",{style:{color:"#89DDFF"}},"    "),t("span",{style:{color:"#C792EA"}},":duration"),t("span",{style:{color:"#89DDFF"}},"="),t("span",{style:{color:"#89DDFF"}},'"'),t("span",{style:{color:"#C3E88D"}},"8000"),t("span",{style:{color:"#89DDFF"}},'"')]),o(`
+`),t("span",{class:"line"},[t("span",{style:{color:"#89DDFF"}},"  />")]),o(`
+`),t("span",{class:"line"},[t("span",{style:{color:"#89DDFF"}},"</"),t("span",{style:{color:"#F07178"}},"template"),t("span",{style:{color:"#89DDFF"}},">")]),o(`
+`),t("span",{class:"line"}),o(`
+`),t("span",{class:"line"},[t("span",{style:{color:"#89DDFF"}},"<"),t("span",{style:{color:"#F07178"}},"script"),t("span",{style:{color:"#89DDFF"}},">")]),o(`
+`),t("span",{class:"line"},[t("span",{style:{color:"#89DDFF","font-style":"italic"}},"import"),t("span",{style:{color:"#A6ACCD"}}," "),t("span",{style:{color:"#89DDFF"}},"{"),t("span",{style:{color:"#F07178"}}," "),t("span",{style:{color:"#A6ACCD"}},"defineComponent"),t("span",{style:{color:"#F07178"}}," "),t("span",{style:{color:"#89DDFF"}},"}"),t("span",{style:{color:"#A6ACCD"}}," "),t("span",{style:{color:"#89DDFF","font-style":"italic"}},"from"),t("span",{style:{color:"#A6ACCD"}}," "),t("span",{style:{color:"#89DDFF"}},"'"),t("span",{style:{color:"#C3E88D"}},"vue"),t("span",{style:{color:"#89DDFF"}},"'"),t("span",{style:{color:"#89DDFF"}},";")]),o(`
+`),t("span",{class:"line"}),o(`
+`),t("span",{class:"line"},[t("span",{style:{color:"#89DDFF","font-style":"italic"}},"export"),t("span",{style:{color:"#A6ACCD"}}," "),t("span",{style:{color:"#89DDFF","font-style":"italic"}},"default"),t("span",{style:{color:"#A6ACCD"}}," "),t("span",{style:{color:"#82AAFF"}},"defineComponent"),t("span",{style:{color:"#A6ACCD"}},"("),t("span",{style:{color:"#89DDFF"}},"{")]),o(`
+`),t("span",{class:"line"},[t("span",{style:{color:"#A6ACCD"}},"  "),t("span",{style:{color:"#F07178"}},"setup"),t("span",{style:{color:"#89DDFF"}},"()"),t("span",{style:{color:"#A6ACCD"}}," "),t("span",{style:{color:"#89DDFF"}},"{")]),o(`
+`),t("span",{class:"line"},[t("span",{style:{color:"#F07178"}},"    "),t("span",{style:{color:"#89DDFF","font-style":"italic"}},"return"),t("span",{style:{color:"#F07178"}}," "),t("span",{style:{color:"#89DDFF"}},"{")]),o(`
+`),t("span",{class:"line"},[t("span",{style:{color:"#F07178"}},"      msg"),t("span",{style:{color:"#89DDFF"}},":"),t("span",{style:{color:"#F07178"}}," "),t("span",{style:{color:"#89DDFF"}},"'"),t("span",{style:{color:"#C3E88D"}},"CountTo Count-to  组件文档示例"),t("span",{style:{color:"#89DDFF"}},"'")]),o(`
+`),t("span",{class:"line"},[t("span",{style:{color:"#F07178"}},"    "),t("span",{style:{color:"#89DDFF"}},"};")]),o(`
+`),t("span",{class:"line"},[t("span",{style:{color:"#F07178"}},"  "),t("span",{style:{color:"#89DDFF"}},"}")]),o(`
+`),t("span",{class:"line"},[t("span",{style:{color:"#89DDFF"}},"}"),t("span",{style:{color:"#A6ACCD"}},")"),t("span",{style:{color:"#89DDFF"}},";")]),o(`
+`),t("span",{class:"line"},[t("span",{style:{color:"#89DDFF"}},"</"),t("span",{style:{color:"#F07178"}},"script"),t("span",{style:{color:"#89DDFF"}},">")]),o(`
+`),t("span",{class:"line"}),o(`
+`),t("span",{class:"line"},[t("span",{style:{color:"#89DDFF"}},"<"),t("span",{style:{color:"#F07178"}},"style"),t("span",{style:{color:"#89DDFF"}},"></"),t("span",{style:{color:"#F07178"}},"style"),t("span",{style:{color:"#89DDFF"}},">")])])])],-1),V=i('<h3 id="c-count-to" tabindex="-1">c-count-to <a class="header-anchor" href="#c-count-to" aria-label="Permalink to &quot;c-count-to&quot;">​</a></h3><p>c-count-to 参数</p><table><thead><tr><th>参数</th><th>类型</th><th>默认</th><th>说明</th></tr></thead><tbody><tr><td>startVal</td><td>number</td><td>0</td><td>开始值</td></tr><tr><td>endVal</td><td>number</td><td>2021</td><td>结束值</td></tr><tr><td>duration</td><td>number</td><td>1500</td><td>持续时间</td></tr><tr><td>autoplay</td><td>boolean</td><td>true</td><td>是否自动</td></tr><tr><td>decimals</td><td>number</td><td>0</td><td>保留精度</td></tr><tr><td>prefix</td><td>string</td><td></td><td>前缀</td></tr><tr><td>suffix</td><td>string</td><td></td><td>后缀</td></tr><tr><td>separator</td><td>string</td><td>,</td><td>分隔符</td></tr><tr><td>decimal</td><td>string</td><td>.</td><td>小数点符号</td></tr><tr><td>color</td><td>string</td><td></td><td>颜色</td></tr><tr><td>useEasing</td><td>boolean</td><td>true</td><td>动画</td></tr><tr><td>transition</td><td>string</td><td>linear</td><td>动画类型</td></tr></tbody></table><p>c-count-to 事件</p><table><thead><tr><th>事件</th><th>类型</th><th>说明</th></tr></thead><tbody><tr><td></td><td></td><td></td></tr><tr><td></td><td></td><td></td></tr><tr><td></td><td></td><td></td></tr></tbody></table><p>c-count-to 插槽</p><table><thead><tr><th>插槽名</th><th>说明</th></tr></thead><tbody><tr><td></td><td></td></tr><tr><td></td><td></td></tr><tr><td></td><td></td></tr></tbody></table>',7);function v(a,r,c,u,d,F){const s=D("render-demo-0"),e=D("demo");return E(),B("div",null,[b,p(e,{customClass:"undefined",sourceCode:`<template>
+  <div>{{ msg }}</div>
+  <c-count-to
+    prefix="$"
+    :color="'#409EFF'"
+    :startVal="1"
+    :endVal="200000"
+    :duration="8000"
+  />
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  setup() {
+    return {
+      msg: 'CountTo Count-to  组件文档示例'
+    };
+  }
+});
+<\/script>
+
+<style></style>
+`},{description:l(()=>[g]),highlight:l(()=>[x]),default:l(()=>[p(s)]),_:1}),V])}const T=A(_,[["render",v]]);export{q as __pageData,T as default};
